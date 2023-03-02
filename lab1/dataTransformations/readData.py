a91b9b9710a242f09f084195513871fded61b43b
krzysiou <krzysztof.kt.tluszcz@gmail.com>
2023-03-02 09:51:32 +0100

    fix: fix read from file logic

--- lab1/dataTransformations/readData.py
+++ lab1/dataTransformations/readData.py
@@ -6,6 +6,6 @@ def readMatrix(file_name):
 
 def readList(file_name):
   with open(file_name, 'r') as file:
-    result = [[num for num in line.split().pop(0)] for line in file]
+    result = [[num for num in line.split()[1:]] for line in file]
   
   return result
