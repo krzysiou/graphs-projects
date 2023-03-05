254574bba96392b9be90b3b005be785c6c37b668
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-03-05 13:20:12 +0100

    feat: convert from NL to NM

--- lab1/dataTransformations/convertData.py
+++ lab1/dataTransformations/convertData.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def convertNeighbourMatrix(neighbourMatrix):
     # code here
     print(1)
@@ -5,7 +8,12 @@ def convertNeighbourMatrix(neighbourMatrix):
 def convertIncidentMatrix(incidentMatrix):
     #code here
     print(1)
+def convertNeighbourList(neighbourList):
+  numberOfVertices = len(neighbourList)
+  neighbourMatrix = np.zeros((numberOfVertices, numberOfVertices))
+  for vertexIndex, vertexNeighbours in enumerate(neighbourList):
+    for neighbour in vertexNeighbours:
+      neighbourMatrix[vertexIndex][neighbour - 1] = 1
 
-def convertNeighbourList(data):
-    #code here
-    print(1)
+  print(neighbourMatrix)
+  return [neighbourMatrix, [0]]
