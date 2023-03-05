f2aa318fa5cd5ea92e870193c1c6919a0ff63921
ur8an5ky <73550318+ur8an5ky@users.noreply.github.com>
2023-03-05 18:39:08 +0100

    feat: add conversions (#2)

--- lab1/dataTransformations/convertData.py
+++ lab1/dataTransformations/convertData.py
@@ -1,41 +1,23 @@
-import numpy as np
-
+from dataTransformations.helperMethods import *
 
 def convertNeighbourMatrix(neighbourMatrix):
-  # code here
-  print(1)
+    #####  NM -> IN
+    incidentMatrix = nm2in(neighbourMatrix)
+    #####  NM -> NL
+    neighbourList = in2nl(incidentMatrix)
+    return incidentMatrix, neighbourList
 
 
 def convertIncidentMatrix(incidentMatrix):
-  # code here
-  print(1)
-
+    #####  IN -> NL
+    neighbourList = in2nl(incidentMatrix)
+    #####  IN -> NM
+    neighbourMatrix = nl2nm(neighbourList)
+    return neighbourList, neighbourMatrix
 
 def convertNeighbourList(neighbourList):
-  numberOfVertices = len(neighbourList)
-
-  neighbourMatrix = np.zeros(
-      (numberOfVertices, numberOfVertices), dtype='int')
-  for vertexIndex, vertexNeighbours in enumerate(neighbourList):
-    for neighbour in vertexNeighbours:
-      if neighbour - 1 >= numberOfVertices:
-        raise ValueError("Wrong neighbour list matrix")
-      neighbourMatrix[vertexIndex][neighbour - 1] = 1
-
-  numberOfEdges = np.count_nonzero(neighbourMatrix == 1) // 2
-  incidentMatrix = np.zeros((numberOfVertices, numberOfEdges), dtype='int')
-
-  edgeCount = 0
-  edgesOfVertex = {}
-  for vertexIndex, vertexNeighbours in enumerate(neighbourList):
-    edgesOfVertex[vertexIndex + 1] = {}
-    for neighbour in vertexNeighbours:
-      if neighbour - 1 > vertexIndex:
-        incidentMatrix[vertexIndex][edgeCount] = 1
-        edgesOfVertex[vertexIndex + 1][neighbour] = edgeCount + 1
-        edgeCount += 1
-      else:
-        edgeIndex = edgesOfVertex[neighbour][vertexIndex + 1]
-        incidentMatrix[vertexIndex][edgeIndex - 1] = 1
-
-  return [neighbourMatrix, incidentMatrix]
+    ######  NL -> NM
+    neighbourMatrix = nl2nm(neighbourList)
+    #####  NL -> IN
+    incidentMatrix = nm2in(neighbourMatrix)
+    return neighbourMatrix, incidentMatrix
--- lab1/main.py
+++ lab1/main.py
@@ -11,28 +11,28 @@ def main(mode, file_name):
     neighbourMatrix = readMatrix(file_name)
     incidentMatrix, neighbourList = convertNeighbourMatrix(neighbourMatrix)
 
-    displayMatrix(incidentMatrix)
-    displayList(neighbourList)
+    # displayMatrix(incidentMatrix)
+    # displayList(neighbourList)
 
     drawGraph(neighbourMatrix)
 
   elif (mode == "IN"):
     incidentMatrix = readMatrix(file_name)
-    neighbourMatrix, neighbourList = convertIncidentMatrix(incidentMatrix)
-
-    displayMatrix(neighbourMatrix)
-    displayList(neighbourList)
+    neighbourList, neighbourMatrix = convertIncidentMatrix(incidentMatrix)
+    
+    # displayMatrix(neighbourMatrix)
+    # displayList(neighbourList)
 
     drawGraph(neighbourMatrix)
 
   elif (mode == "NL"):
     neighbourList = readList(file_name)
     neighbourMatrix, incidentMatrix = convertNeighbourList(neighbourList)
+    
+    # displayMatrix(neighbourMatrix)
+    # displayMatrix(incidentMatrix)
 
-    displayMatrix(neighbourMatrix)
-    displayMatrix(incidentMatrix)
-
-    drawGraph(neighbourList)
+    drawGraph(neighbourMatrix)
 
   else:
     sys.exit('Please provide valide mode, [NM, IN, NL]')
