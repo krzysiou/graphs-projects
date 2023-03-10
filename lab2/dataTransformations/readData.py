021f2d2956f40fce97f903a581ee6cf1f332d965
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-03-10 20:29:45 +0100

    feat: read one line of numbers from file

--- lab2/dataTransformations/readData.py
+++ lab2/dataTransformations/readData.py
@@ -11,3 +11,9 @@ def readList(file_name):
 
   return result
 
+
+def readLine(file_name):
+  with open(file_name, 'r') as file:
+    result = [int(num) for num in file.readline().split()]
+
+  return result
