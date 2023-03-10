ef7cd51285449c94bf00748c2861112ca9da6dd0
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-03-10 20:29:21 +0100

    Copy from last lab

---
+++ lab2/dataTransformations/readData.py
@@ -0,0 +1,13 @@
+def readMatrix(file_name):
+  with open(file_name, 'r') as file:
+    result = [[int(num) for num in line.split()] for line in file]
+
+  return result
+
+
+def readList(file_name):
+  with open(file_name, 'r') as file:
+    result = [[int(num) for num in line.split()[1:]] for line in file]
+
+  return result
+
---
+++ lab2/utils.py
@@ -0,0 +1,46 @@
+import igraph as ig
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def generateEdgesList(neighbourList):
+  edges = []
+  for index, neighbours in enumerate(neighbourList):
+    edges.extend((index, neighbour) for neighbour in neighbours)
+
+  return edges
+
+
+def matrixOfZeros(sizeX, sizeY):
+  matrix = []
+
+  for _ in range(sizeX):
+    temp = []
+    for _ in range(sizeY):
+      temp.append(0)
+    matrix.append(temp)
+
+  return matrix
+
+
+def drawGraph(neighbourList):
+  g = ig.Graph(len(neighbourList), generateEdgesList(neighbourList))
+  g.vs['id'] = [i + 1 for i in range(len(neighbourList))]
+
+  _, ax = plt.subplots()
+  ig.plot(
+    g,
+    target=ax,
+    layout="circle",
+    vertex_size=0.25,
+    vertex_color="#d9d9ff",
+    vertex_frame_width=1.0,
+    vertex_frame_color="#1414ff",
+    vertex_label_size=16.0,
+    vertex_label=g.vs['id'],
+    edge_width=2,
+    edge_color="#000"
+      )
+
+  plt.show()
+
