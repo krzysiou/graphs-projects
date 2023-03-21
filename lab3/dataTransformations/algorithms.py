f8e73a20385ee9d8a5e945fb6c7d8d2bde965d4d
Mateusz Cichostępski <87618425+Matiixx@users.noreply.github.com>
2023-03-21 19:45:50 +0100

    feat: implement task3, minor fixes in task2 (#9)

--- lab3/dataTransformations/algorithms.py
+++ lab3/dataTransformations/algorithms.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 from utils import *
 
+
 def initializeAtributes(graphLength, s):
     ds = []
     ps = []
@@ -12,16 +13,19 @@ def initializeAtributes(graphLength, s):
     ds[s] = 0
     return ds, ps
 
+
 def relax(u, v, w):
     print()
+
 
 def updateMatrixWithValues(neighbourMatrix, edges, edgesValues):
     neighbourMatrixWithValues = copy.deepcopy(neighbourMatrix)
     for i, edge in enumerate(edges):
         neighbourMatrixWithValues[edge[0]][edge[1]] = edgesValues[i]
-        neighbourMatrixWithValues[edge[1]][edge[0]] = edgesValues[i] 
+        neighbourMatrixWithValues[edge[1]][edge[0]] = edgesValues[i]
 
-    return neighbourMatrixWithValues 
+    return neighbourMatrixWithValues
+
 
 def minDistance(d, S):
     minDist = sys.maxsize
@@ -31,15 +35,34 @@ def minDistance(d, S):
             minIndex = i
     return minIndex
 
-def printSolution(d):
-    print("Vertex \t Distance from Source")
+
+def getPath(start, end, p):
+    if start == end:
+        return [start + 1]
+    path = [end + 1]
+    step = p[end]
+    while step != start:
+        path.append(step + 1)
+        step = p[step]
+    path.append(start + 1)
+    path.reverse()
+    return path
+
+
+def printSolution(d, p, start):
+    print(p)
+    print(f'START: s = {start+1}')
     for node in range(len(d)):
-        print(node + 1, "\t\t", d[node])
+        print(f'd({node+1}) = {d[node]} ==> [', end='')
+        print(' - '.join(map(str, getPath(start, node, p))), end='')
+        print("]")
+
 
 def dijkstra(neighbourList, edgesValues, s):
     neighbourMatrix = convertToNeighbourMatrix(neighbourList)
     edges = generateEdgesList(neighbourList)
-    neighbourMatrixWithValues = updateMatrixWithValues(neighbourMatrix, edges, edgesValues)
+    neighbourMatrixWithValues = updateMatrixWithValues(
+        neighbourMatrix, edges, edgesValues)
     d, p = initializeAtributes(len(neighbourList), s)
     S = [False for _ in range(len(neighbourList))]
 
@@ -50,7 +73,9 @@ def dijkstra(neighbourList, edgesValues, s):
             if (neighbourMatrixWithValues[u][v] > 0 and S[v] == False
                     and d[v] > d[u] + neighbourMatrixWithValues[u][v]):
                 d[v] = d[u] + neighbourMatrixWithValues[u][v]
-    printSolution(d)    
+                p[v] = u
+    return [d, p]
+
 
 def convertToNeighbourMatrix(neighbourList):
     neighbourMatrix = matrixOfZeros(len(neighbourList), len(neighbourList))
@@ -62,3 +87,11 @@ def convertToNeighbourMatrix(neighbourList):
                 neighbourMatrix[int(neighbourList[i][j])][i] = 1
 
     return neighbourMatrix
+
+
+def lengthMatrix(neighbourList, edgesValues):
+    matrix = matrixOfZeros(len(neighbourList), len(neighbourList))
+    for startNode in range(len(neighbourList)):
+        d, _ = dijkstra(neighbourList, edgesValues, startNode)
+        matrix[startNode] = d
+    return matrix
