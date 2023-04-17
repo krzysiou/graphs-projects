863657e987678cdf9d0f6024fd1275d2c63140cd
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-04-17 17:47:40 +0200

    feat: generate vertices and edges for each layer

---
+++ lab5/dataTransformations/algorithms.py
@@ -0,0 +1,50 @@
+import sys
+import numpy as np
+import copy
+import random
+
+
+def generateFlowNetwork(N):
+    graphLayers = generateVerticesLayers(N)
+    graphEdges = generateGraphEdges(graphLayers)
+
+    return [graphLayers, graphEdges]
+
+
+def generateVerticesLayers(N):
+    graphLayers = {}
+    graphLayers[0] = ["s"]
+
+    count = 0
+    for i in range(1, N + 1):
+        numberOfVerticesInLayer = random.randint(1, N)
+        graphLayers[i] = []
+        for j in range(numberOfVerticesInLayer):
+            graphLayers[i].append(count)
+            count += 1
+
+    graphLayers[N + 1] = ["t"]
+    return graphLayers
+
+
+def generateGraphEdges(graphLayers):
+    edgesList = []
+    N = len(graphLayers)
+    edgesList.extend(["s", i] for i in graphLayers[1])
+
+    for i in range(1, N - 2):
+        currentLayerEdges = graphLayers[i]
+        nextLayerEdges = graphLayers[i + 1]
+
+        for _ in range(len(currentLayerEdges)):
+            randomEdge = [
+                random.choice(currentLayerEdges),
+                random.choice(nextLayerEdges),
+            ]
+            if randomEdge in edgesList:
+                continue
+            edgesList.append(randomEdge)
+
+    edgesList.extend([i, "t"] for i in graphLayers[N - 2])
+
+    return edgesList
