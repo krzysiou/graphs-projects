4ad8d0e45195e87769a34f9993dc23823d46cc67
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-04-17 18:41:27 +0200

    feat: draw flow network

--- lab5/utils.py
+++ lab5/utils.py
@@ -53,6 +53,36 @@ def drawGraphWithValues(edgesList, edgesValues):
     plt.show()
 
 
+def drawFlowNetwork(edgesList, layers):
+    print(edgesList)
+    node_count = (layers[len(layers) - 2]
+                  [len(layers[len(layers) - 2]) - 1]) + 2
+
+    g = ig.Graph(node_count, edgesList, directed=True)
+
+    g.vs["id"] = ["s"] + [i + 1 for i in range(node_count - 2)] + ["t"]
+
+    g.vs['frame width'] = [2.5] + [1.0] * (node_count - 2) + [2.5]
+
+    _, ax = plt.subplots()
+    ig.plot(
+        g,
+        target=ax,
+        layout="tree",
+        vertex_size=0.4,
+        vertex_color="#d9d9ff",
+        vertex_frame_width=g.vs['frame width'],
+        vertex_frame_color="#1414ff",
+        vertex_label_size=16.0,
+        vertex_label=g.vs["id"],
+        edge_width=2,
+        edge_color="#000",
+    )
+
+    plt.show()
+    pass
+
+
 def convertEdgesToNeighbourList(edges):
     maxList = map(max, edges)
     node_count = max(maxList)
