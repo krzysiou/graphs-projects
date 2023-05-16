e350f28a55925ae3007f8e455701cf23d7de3697
Kuba <urbanski.jakub77@gmail.com>
2023-05-16 23:23:37 +0200

    fix: minor changes lab2

--- lab2/main.py
+++ lab2/main.py
@@ -5,31 +5,32 @@ from dataTransformations.algorithms import *
 from utils import *
 
 
-def main(mode, file_name):
+def main(mode, second_argument):
     if mode == "task1":
-        seqArray = readLine(file_name)
+        seqArray = readLine(second_argument)
         print(f"Graph sequence: {degree_seq(seqArray)}")
         drawGraph(constructNLFromSeq(seqArray))
 
     elif mode == "task2":
-        seqArray = readLine(file_name)
+        seqArray = readLine(second_argument)
         numberOfRands = int(input("Write number of randomizations: "))
         neighbourList = constructNLFromSeq(seqArray)
         randEdges = randomizeEdges(neighbourList, numberOfRands, verbose=True)
         drawGraph(randEdges, "EL")
 
     elif mode == "task3":
-        seqArray = readLine(file_name)
+        seqArray = readLine(second_argument)
         neighbourList = constructNLFromSeq(seqArray)
 
         print(neighbourList)
 
         comp = calculateIntegrityArray(seqArray, neighbourList)
-        print(f"Max integrity number: {max(comp)}")
+        connectedComponents(comp)
         drawGraph(neighbourList)
 
     elif mode == "task4":
-        eulerNeighbourList = generateEulerGraph(5, 6)
+        numOfEdges = int(input("Write number of edges: "))
+        eulerNeighbourList = generateEulerGraph(int(second_argument), numOfEdges)
         eulerCycle = findEulerCycle(copy.deepcopy(eulerNeighbourList))
 
         print(eulerCycle)
@@ -37,12 +38,13 @@ def main(mode, file_name):
         drawGraphV2(eulerNeighbourList)
 
     elif mode == "task5":
-        regularNeighbourList = generateRegularGraph(7, 2)
+        k = int(input("Write number of edges from each vertex: "))
+        regularNeighbourList = generateRegularGraph(int(second_argument), k)
 
         drawGraphV2(regularNeighbourList)
 
     elif mode == "task6":
-        seqArray = readLine(file_name)
+        seqArray = readLine(second_argument)
         neighbourList = constructNLFromSeq(seqArray)
 
         checkIfHamiltonian(neighbourList)
@@ -50,7 +52,7 @@ def main(mode, file_name):
 
     else:
         sys.exit(
-            "Please provide valide mode, [task1, task2, task3, task4, task5, task6]"
+            "Please provide valid mode, [task1, task2, task3, task4, task5, task6]"
         )
 
 
@@ -59,7 +61,7 @@ if __name__ == "__main__":
 
     if len(programArguments) != 3:
         sys.exit(
-            "Please provide two arguments, first is the mode [[task1, task2, task3, task4, task5, task6], second one is input file name."
+            "Please provide two arguments, for modes [task1, task2, task3, task6] second one is input file name, for [task4, task5] second one is number of vertexes."
         )
 
     main(programArguments[1], programArguments[2])
