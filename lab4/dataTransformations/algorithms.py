e3d0362a90fa40208691158ac9710e45ef2b86b9
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-06-10 15:23:33 +0200

    fix app crashing when 0 probability or nodeCount 1

--- lab4/dataTransformations/algorithms.py
+++ lab4/dataTransformations/algorithms.py
@@ -9,8 +9,32 @@ from .dijkstra import dijkstra
 def generateDigraph(node_count, probability):
     if probability < 0 or probability > 1:
         sys.exit("Wrong randomization arguments")
+    if node_count < 1:
+        sys.exit("Wrong node count")
 
     edges = []
+
+    if probability == 0 or node_count == 1:
+        g = ig.Graph(node_count, directed=True)
+        g.vs["id"] = [i + 1 for i in range(node_count)]
+
+        _, ax = plt.subplots()
+        ig.plot(
+            g,
+            target=ax,
+            layout="circle",
+            vertex_size=0.25,
+            vertex_color="#d9d9ff",
+            vertex_frame_width=1.0,
+            vertex_frame_color="#1414ff",
+            vertex_label_size=16.0,
+            vertex_label=g.vs["id"],
+            edge_width=2,
+            edge_color="#000",
+        )
+
+        plt.show()
+        sys.exit()
 
     while True:
         for i in range(0, node_count):
