b1f8653fd0dbbd4c9b183f42a38aae25cb97a4a8
TRZCodes <jtrzesnie@student.agh.edu.pl>
2023-06-14 22:30:03 +0200

    Slightly modified lab6 + added plot script

--- lab6/main.py
+++ lab6/main.py
@@ -4,12 +4,17 @@ from dataTransformations.algorithms import *
 from utils import *
 
 def main(mode, nodesCount=12, propability=0.4):
-    edges = generateDigraph(nodesCount, propability)
-    NL = convertEdgesToNeighbourList(edges)
-    #NL = [[4,5,8], [0,2,5], [1,3,4,11], [2,4,7,8,10], [2,6,7,8], [1,6], [4,5,7], [3,6,8,11], [3,4,7,9], [8], [3,8], [0,7]]
+    #edges = generateDigraph(nodesCount, propability)
+    #NL = convertEdgesToNeighbourList(edges)
+    #zakomentowana wersja z generacją losowego digrafu
+    #poniżej krawędzie i lista sąsiedztwa dla podanego przykładu 
+    edges = [[0,4], [0,5], [0,8], [1,0], [1,2], [1,5], [2,1], [2,3] ,[2,4] , [2,11] ,[3,2] ,[3,4] ,[3,7] ,[3,8] ,[3,10] ,[4,2] ,[4,6] ,[4,7] ,[4,8] ,[5,1] ,[5,6] ,[6,4] , [6,5] ,[6,7] ,[7,3] ,[7,6] ,[7,8] ,[7,11] , [8,3] ,[8,4] ,[8,7] ,[8,9] ,[9,8] ,[10,3] ,[10,8] ,[11,0] ,[11,7]]
+    NL = [[4,5,8], [0,2,5], [1,3,4,11], [2,4,7,8,10], [2,6,7,8], [1,6], [4,5,7], [3,6,8,11], [3,4,7,9], [8], [3,8], [0,7]]
     if mode == "pagerank1":
+        #drawGraph(edges)
         pageRank1(NL, 0.15)
     elif mode == "pagerank2":
+        #drawGraph(edges)
         pageRank2(NL, 0.15)
     else:
         sys.exit("Please provide valide mode, [pagerank1, pagerank2]")
